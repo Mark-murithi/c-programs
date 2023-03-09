ea622475a8f467ec33b6832eac4830a790b8ca82
Mark-murithi <123064791+Mark-murithi@users.noreply.github.com>
2023-03-09 15:00:40 +0300

    Add files via upload

---
+++ array marks.c
@@ -0,0 +1,12 @@
+//program to print 10 marks array
+#include<stdio.h>
+int main()
+{
+int i=0;
+int marks[10]={15,20,30,40,50,60,70,80,90,99};
+for(i=0;i<10;i++)
+{
+printf("%d \n",marks[i]);
+}
+return 0;
+}
